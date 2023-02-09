4ad5a4061881e5eb945d64b96d058a0d2ec1df93
SLStudioGithub <consultant-teams@fox-hound.jp>
2023-02-09 18:01:21 +0900

    init

---
+++ src/views/DetailView.vue
@@ -0,0 +1,124 @@
+<template>
+  <div>
+    <div class="top-area">
+      <p class="top-title">予定詳細</p>
+    </div>
+    <div class="main-area">
+      <div class="my-5">
+        <input
+          class="w-[500px]"
+          placeholder="タイトル"
+          type="text"
+          v-model="title"
+        />
+      </div>
+      <div class="my-5">
+        <textarea
+          class="w-[500px]"
+          placeholder="内容"
+          cols="30"
+          rows="10"
+          v-model="description"
+        ></textarea>
+      </div>
+      <div class="flex justify-between my-5">
+        <input class="w-[230px]" type="date" v-model="startDate" />
+        <p>~</p>
+        <input class="w-[230px]" type="date" v-model="endDate" />
+      </div>
+      <div class="my-5">
+        <button class="update-button" @click="updateTodo()">更新</button>
+        <button class="delete-button" @click="deleteTodo()">削除</button>
+      </div>
+    </div>
+  </div>
+</template>
+<script lang="ts" setup>
+import { ref } from "vue";
+import { useRoute, useRouter } from "vue-router";
+import { Todo } from "@/interface/todo";
+
+const route = useRoute();
+const router = useRouter();
+
+/**
+ * ローカルストレージのデータを取得・保持
+ */
+const dataList = ref<Todo[]>(
+  JSON.parse(window.localStorage.getItem("todoList") ?? "")
+);
+const data: Todo | undefined = dataList.value.find(
+  (data: Todo) => data.id === Number(route.params.id)
+);
+
+const index: number = dataList.value.findIndex(
+  (data: Todo) => data.id === Number(route.params.id)
+);
+
+const title = ref(data?.title);
+const description = ref(data?.description);
+const startDate = ref(data?.startDate);
+const endDate = ref(data?.endDate);
+
+/**
+ * 予定の更新処理
+ */
+const updateTodo = () => {
+  const dataset: Todo = {
+    id: data?.id ?? 0,
+    status: data?.status ?? 0,
+    title: title?.value ?? "",
+    description: description?.value ?? "",
+    startDate: startDate?.value ?? "",
+    endDate: endDate?.value ?? "",
+  };
+  let dataList: Todo[] = JSON.parse(
+    window.localStorage.getItem("todoList") ?? ""
+  );
+
+  dataList[index] = dataset;
+  window.localStorage.setItem("todoList", JSON.stringify(dataList));
+  router.push("/");
+};
+
+/**
+ * 予定の削除処理
+ */
+const deleteTodo = () => {
+  let dataList: Todo[] = JSON.parse(
+    window.localStorage.getItem("todoList") ?? ""
+  );
+  dataList.splice(index, 1);
+  window.localStorage.setItem("todoList", JSON.stringify(dataList));
+  router.push("/");
+};
+</script>
+<style lang="postcss" scoped>
+.top-area {
+  @apply flex justify-center items-center my-7;
+}
+.main-area {
+  @apply w-[500px] mx-auto;
+}
+.top-title {
+  @apply my-5 text-2xl font-semibold;
+}
+input {
+  @apply px-3 py-2 border border-solid border-gray-300 rounded outline-none;
+}
+textarea {
+  @apply px-3 py-2 border border-solid border-gray-300 rounded outline-none resize-none;
+}
+.update-button {
+  @apply bg-green-700 text-white;
+}
+.delete-button {
+  @apply bg-red-700 text-white;
+}
+
+.update-button,
+.delete-button {
+  transition: all ease 0.2s;
+  @apply font-semibold py-2 px-3 rounded mx-5 w-[100px];
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,118 @@
+<template>
+  <div class="top-area">
+    <p class="top-title">TOP (予定一覧表示)</p>
+  </div>
+  <div class="main-area">
+    <FilterList
+      :filterStatus="filterStatus"
+      @changeFilter="changeFilter"
+    ></FilterList>
+    <div class="flex justify-center">
+      <ul class="todo-list">
+        <li class="flex justify-end">
+          <button class="add-button" @click="$router.push('/add')">
+            新規登録
+          </button>
+        </li>
+        <li v-if="!todoList.length">登録されている予定はありません。</li>
+        <li v-for="todo in todoList" :key="todo.id">
+          <TodoList :todo="todo" @updateStatus="updateStatus"></TodoList>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+<script lang="ts" setup>
+import FilterList from "@/components/FilterList.vue";
+import TodoList from "@/components/TodoList.vue";
+import { ref } from "vue";
+import { Todo } from "@/interface/todo";
+
+const dataList = ref<Todo[]>(
+  JSON.parse(window.localStorage.getItem("todoList") ?? "")
+);
+
+/**
+ * フィルターのステータス
+ */
+const filterStatus = ref("3");
+
+const todoList = ref<Todo[]>(
+  dataList.value.sort(function (a, b) {
+    if (a.status > b.status) return 1;
+    if (a.status < b.status) return -1;
+
+    if (a.endDate > b.endDate) return 1;
+    if (a.endDate < b.endDate) return -1;
+    return 0;
+  })
+);
+
+/**
+ * ステータスの更新処理
+ *
+ * @param {number} id
+ * @param {number} status
+ */
+const updateStatus = (id: number, status: number) => {
+  let todo: Todo | undefined = dataList.value.find((todo) => todo.id === id);
+  if (todo !== undefined && status < 2) {
+    // ステータスを更新
+    todo.status = status + 1;
+    const index = dataList.value.findIndex((todo) => todo.id === id);
+    dataList.value[index] = todo;
+    // ローカルストレージへ保存
+    window.localStorage.setItem("todoList", JSON.stringify(dataList.value));
+    changeFilter(filterStatus.value);
+  }
+};
+
+/**
+ * フィルターの更新処理
+ *
+ * @param {string} status
+ */
+const changeFilter = (status: string) => {
+  filterStatus.value = status;
+  todoList.value = dataList.value;
+  todoList.value.sort(function (a, b) {
+    if (a.status > b.status) return 1;
+    if (a.status < b.status) return -1;
+
+    if (a.endDate > b.endDate) return 1;
+    if (a.endDate < b.endDate) return -1;
+    return 0;
+  });
+  if (Number(status) !== 3) {
+    // 対象のステータス以外を除外
+    todoList.value = todoList.value.filter(
+      (todo) => todo.status === Number(status)
+    );
+  }
+};
+</script>
+<style lang="postcss" scoped>
+.top-area {
+  @apply flex justify-center items-center my-7;
+}
+.top-title {
+  @apply my-5 text-2xl font-semibold;
+}
+.add-button {
+  transition: all ease 0.2s;
+  @apply border border-solid font-medium py-2 px-3 rounded mx-2;
+}
+.add-button:hover {
+  transition: all ease 0.2s;
+  @apply bg-gray-500 text-white;
+}
+.detail-area .change-progress-area {
+  @apply w-1/5;
+}
+.todo-list li {
+  @apply border-b border-solid py-4 w-[700px];
+}
+.todo-list li:last-child {
+  @apply border-none;
+}
+</style>
